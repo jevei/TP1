5caffb844f590942810ad010561ade5133875081
Jérémy Veillette <1830942@cshawi.ca>
2020-09-15 09:30:00 -0400

    s

--- FileScanner/ViewModels/MainViewModel.cs
+++ FileScanner/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,8 @@ namespace FileScanner.ViewModels
     {
         private string selectedFolder;
         private ObservableCollection<string> folderItems = new ObservableCollection<string>();
-         
+        private ObservableCollection<Item> items = new ObservableCollection<Item>();
+
         public DelegateCommand<string> OpenFolderCommand { get; private set; }
         public DelegateCommand<string> ScanFolderCommand { get; private set; }
 
@@ -23,6 +25,14 @@ namespace FileScanner.ViewModels
             set 
             { 
                 folderItems = value;
+                OnPropertyChanged();
+            }
+        }
+        public ObservableCollection<Item> Items {
+            get => items;
+            set
+            {
+                items = value;
                 OnPropertyChanged();
             }
         }
@@ -60,22 +70,69 @@ namespace FileScanner.ViewModels
             }
         }
 
-        private void ScanFolder(string dir)
+        private async void ScanFolder(string dir)
         {
-            FolderItems = new ObservableCollection<string>(GetDirs(dir));
-            
-            foreach (var item in Directory.EnumerateFiles(dir, "*"))
+            var watch = Stopwatch.StartNew();
+            try
             {
-                FolderItems.Add(item);
+                await Scan(dir);
+            }
+            catch (System.UnauthorizedAccessException ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                
+            }
+            watch.Stop();
+        }
+        private async Task Scan(string dir)
+        {
+            //FolderItems = new ObservableCollection<string>(GetDirs(dir));
+            Items = new ObservableCollection<Item>();
+            var directory = Directory.EnumerateDirectories(dir, "*");
+            var file = Directory.EnumerateFiles(dir, "*");
+
+            foreach (var item in directory)
+            {
+                //FolderItems.Add(item);
+                //Item temp = new Item { Name = item, Image = "Image/temp-folder.jpg" };
+                Item temp = await Task.Run(() => ItemAdd(item, "Image/temp-folder.jpg"));
+                Items.Add(temp);
+            }
+            foreach (var item in file)
+            {
+                //FolderItems.Add(item);
+                //WebsiteDataModel ws = await Task.Run(() => DownloadWebsite(site));
+                //Item temp = new Item { Name = item, Image = "Image/directory.jpg" };
+                Item temp = await Task.Run(() => ItemAdd(item, "Image/directory.jpg"));
+                Items.Add(temp);
             }
         }
-
+        private Item ItemAdd(string n, string i)
+        {
+            Item output = new Item();
+            output.Name = n;
+            output.Image = i;
+            return output;
+        }
         IEnumerable<string> GetDirs(string dir)
         {            
             foreach (var d in Directory.EnumerateDirectories(dir, "*"))
             {
                 yield return d;
+
+                foreach (var f in Directory.EnumerateFiles(d, "*"))
+                {
+                    yield return f;
+                }
             }
+        }
+        public class Item
+        {
+            public string Image { get; set; }
+            public string Name { get; set; }
         }
 
         ///TODO : Tester avec un dossier avec beaucoup de fichier
